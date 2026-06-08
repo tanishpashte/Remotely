c2c0ff6ad41238fe31fb45226f017bf7d851c089
Tanish <tanishpashte.24@gmail.com>
2026-06-08 17:54:15 +0530

    mask automation flags to evade captchas

--- backend/worker.js
+++ backend/worker.js
@@ -44,11 +44,16 @@ wss.on('connection', async (ws) => {
     console.log('Launching headless Chromium instance...');
     browser = await chromium.launch({
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-blink-features=AutomationControlled'
+      ]
     });
 
     context = await browser.newContext({
-      viewport: { width: 1280, height: 720 }
+      viewport: { width: 1280, height: 720 },
+      userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
     });
 
     page = await context.newPage();
